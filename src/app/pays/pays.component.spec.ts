a16fddff824996807957807e9acc0e813cf226e6
EpsyLaZ <greg_desch@hotmail.fr>
2023-05-15 20:49:25 +1100

    fin

--- src/app/pays/pays.component.spec.ts
+++ src/app/pays/pays.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PaysComponent } from './pays.component';
+import { fromEvent, debounceTime, map } from 'rxjs';
 
 describe('PaysComponent', () => {
   let component: PaysComponent;
@@ -20,4 +21,51 @@ describe('PaysComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
+
+ngAfterViewInit() {
+  this.sub = fromEvent(this.inputText.nativeElement, 'keyup')
+  .pipe(
+    debounceTime(500),
+    map((x) => this.inputText.nativeElement.value)
+  )
+  .subscribe((x) => {
+    if (x.trim().length == 0) {
+      this.currentCountries = [];
+    } else {
+      this.currentCountries = this.countries.filter((y) => 
+        y.libelle.toLowerCase().startsWith(x.toLowerCase())
+      );
+    }
+  });
+}
+
+onBlur() {
+  let meComponent = this;
+  setTimeout(function () {
+    meComponent.currentCountries = [];
+  }, 150)
+}
+
+onFocus() {
+  if (history.inputText.nativeElement.value.trim().length > 0) {
+    this.currentCountries = this.countries.filter((y) => 
+      y.libelle
+        .toLowerCase()
+        .startsWith(this.inputText.nativeElement.value.toLowerCase())
+        );
+  }
+}
+
+selectCountry(event) {
+  this.inputText.nativeElement.value = event?.target.innerText;
+}
+
+ngOnDestroy() {
+  this.sub.unsubscribe();
+}
+
+
+function ngAfterViewInit() {
+  throw new Error('Function not implemented.');
+}
+})
--- src/app/pays/pays.component.ts
+++ src/app/pays/pays.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-pays',
   templateUrl: './pays.component.html',
-  styleUrls: ['./pays.component.scss']
+  styleUrls: ['./pays.component.css']
 })
-export class PaysComponent {
+
+export class PaysComponent implements AfterViewInit, onDestroy {
+
+  sub : Subscriptions;
+
+  @ViewChild('input')
+  inputText : ElementRef;
+
+  countries : Array<Country> = [
+    {
+      code: 'FR',
+      libelle: 'France',
+    },
+    {
+      code: 'NC',
+      libelle: 'Nouvelle-Caledonie'
+    },
+    {
+      code: 'AU',
+      libelle: 'Australie'
+    }
+  ];
+
+  currentCountries: Array<Country> = [];
+
+  constructor() {}
 
 }
+
